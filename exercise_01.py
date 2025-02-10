8e9632da16a2d15f5b3f14aca0c65ead82dbafe6
richiemelcabantog22 <cabantogrichiemel_bsit@plmun.edu.ph>
2025-02-10 15:21:04 +0800

    Submitting Python Week 02 - Session 01 - Exercise 01

---
+++ exercise_01.py
@@ -0,0 +1,14 @@
+count = 10                  # 10 is an integer literal
+decimal_value = 3.14        # 3.14 is a float literal
+message = "Hello, Python!"   # "Hello, Python!" is a string literal
+is_active = True              # True is a boolean literal
+result = None                # None is the None literal
+count = 10                    # or any integer literal
+total_count = 20              # or any other integer literal
+
+print("Integer (count):", count)
+print("Integer (total_count):", total_count)
+print("Decimal:", decimal_value)
+print("Text:", message)
+print("Boolean:", is_active)
+print("None Value:", result)
